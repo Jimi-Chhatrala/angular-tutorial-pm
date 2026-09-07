0a28801ccaaab66ff4b8a908cd532aff4b527d96
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-09-07 20:58:12 +0530

    4-angular-property-binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
+  public newId = 'ExampleId';
+  public isDisabled = true;
   protected readonly title = signal('angular-tutorial-pm');
 }
