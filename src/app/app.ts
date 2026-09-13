469b3d8291fb495a4bd28fff1dbb21a679ea9dbe
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-09-13 17:27:33 +0530

    6-angular-ngclass-directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [NgClass],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('angular-tutorial-pm');
+  public condition = true;
+  public cssClass = 'myColor myWriting';
+  public cssArray = ['myColor', 'myWriting'];
+  public myNewClass = new myClass();
+}
+
+class myClass {
+  myColor = true;
+  myWriting = false;
 }
