2a8430c69864b4913a4df065192a4c6bf6fe6921
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-09-26 13:49:57 +0530

    13-ngfor-directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [NgFor, NgIf],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('angular-tutorial-pm');
+  fruits = ['Apple', 'Banana', 'Carrot', 'Grapes', 'Mango'];
+  persons = [
+    {
+      id: 1,
+      name: 'Abc',
+      age: 18,
+      isMarried: true,
+      active: false,
+    },
+    {
+      id: 2,
+      name: 'Def',
+      age: 2,
+      isMarried: false,
+      active: true,
+    },
+    {
+      id: 3,
+      name: 'Ghi',
+      age: 32,
+      isMarried: true,
+      active: true,
+    },
+  ];
+  users = ['John', 'Alice', 'Bob'];
+  selectUser(user: string) {
+    console.log(user);
+    alert(`You have click on user: ${user}`);
+  }
+  fruits1 = ['Apple', 'Banana', 'Mango', 'Orange'];
 }
